edc08c781b863bce512bd50abcd26329455cb0bd
kazimpal87 <kazim.pal@yoti.com>
2019-08-09 12:36:17 +0100

    wc

--- fcn/models.py
+++ fcn/models.py
@@ -6,14 +6,14 @@ def get_vgg16_pooling_layer(layer_num):
     output = tf.get_default_graph().get_tensor_by_name(name)
     return output
 
-def conv_helper(inputs, filters, kernel_size, name, do_dropout=True):
+def conv2d_helper(inputs, filters, kernel_size, name, do_dropout=True):
     x = tf.layers.conv2d(inputs, filters, kernel_size, padding='same', activation='relu', name=name)
     if do_dropout:
         x = tf.layers.dropout(x)
     return x
 
 def upsample_helper(inputs, filters, scale, name):
-    x = tf.layers.conv2d_transpose(inputs, nb_classes, (scale,scale), strides=(scale,scale), padding='valid', activation=None, name=name)
+    x = tf.layers.conv2d_transpose(inputs, filters, (scale,scale), strides=(scale,scale), padding='valid', activation=None, name=name)
     return x
 
 def fcn_32(sess, inputs, nb_classes):    
@@ -22,7 +22,7 @@ def fcn_32(sess, inputs, nb_classes):
 
     # Replace fc layers with conv layers
     conv6 = conv2d_helper(vgg16_pretrained, 4096, (7,7), name='conv6')
-    conv7 = conv2d_helper(conv7, 4096, (1,1), name='conv7')
+    conv7 = conv2d_helper(conv6, 4096, (1,1), name='conv7')
     conv7_score = conv2d_helper(conv7, nb_classes, (1,1), name='conv7_score', do_dropout=False)
 
     final_score = upsample_helper(conv7_score, nb_classes, 32, name='final_score')
@@ -34,7 +34,7 @@ def fcn_16(sess, inputs, nb_classes):
     
     # Replace fc layers with conv layers
     conv6 = conv2d_helper(vgg16_pretrained, 4096, (7,7), name='conv6')
-    conv7 = conv2d_helper(conv7, 4096, (1,1), name='conv7')
+    conv7 = conv2d_helper(conv6, 4096, (1,1), name='conv7')
     conv7_score = conv2d_helper(conv7, nb_classes, (1,1), name='conv7_score', do_dropout=False)
 
     # Upsample conv7    
@@ -57,7 +57,7 @@ def fcn_8(sess, inputs, nb_classes):
     
     # Replace fc layers with conv layers
     conv6 = conv2d_helper(vgg16_pretrained, 4096, (7,7), name='conv6')
-    conv7 = conv2d_helper(conv7, 4096, (1,1), name='conv7')
+    conv7 = conv2d_helper(conv6, 4096, (1,1), name='conv7')
     conv7_score = conv2d_helper(conv7, nb_classes, (1,1), name='conv7_score', do_dropout=False)
 
     # Upsample conv7    
@@ -66,11 +66,11 @@ def fcn_8(sess, inputs, nb_classes):
     # Get pool4 and class scores
     pool4 = get_vgg16_pooling_layer(4)
     pool4_score = conv2d_helper(pool4, nb_classes, (1,1), name='pool4_score')
-    pool4_score_x2 = upsample_helper(pool4_conv7_score, nb_classes, 2, name='pool4_score_x2')
+    pool4_score_x2 = upsample_helper(pool4_score, nb_classes, 2, name='pool4_score_x2')
 
     # Get pool3 and class scores
     pool3 = get_vgg16_pooling_layer(3)
-    pool3_score = conv2d_helper(pool4, nb_classes, (1,1), name='pool3_score')
+    pool3_score = conv2d_helper(pool3, nb_classes, (1,1), name='pool3_score')
 
     # Compute sum
     sum_score = tf.math.add_n([conv7_score_x4, pool4_score_x2, pool3_score])

c4ec0e5959c184fd8912dbccc77e22100de4badb
kazimpal87 <kazim.pal@yoti.com>
2019-08-09 13:12:08 +0100

    Change dataset to allow validation

--- data_iterator.py
+++ data_iterator.py
@@ -2,57 +2,83 @@ import os
 import numpy as np
 from PIL import Image
 import tensorflow as tf
-import matplotlib.pyplot as plt
 
-def resize_image_to_multiple(image, size_mult):
-    image_shape = tf.shape(image)
-    h = image_shape[0]
-    w = image_shape[1]
-    h = (h // size_mult) * size_mult
-    w = (w // size_mult) * size_mult    
-    image = tf.image.resize_images(image, [h, w])
-    return image
+class PascalDataset(object):
 
-def load_input_image(path, size_mult):
-    image = tf.read_file(path)
-    image = tf.image.decode_jpeg(image, channels=3)
-    image = tf.image.convert_image_dtype(image, tf.float32)
-    image = resize_image_to_multiple(image, size_mult)
-    return image
+    def __init__(self, image_dir, seg_dir, file_list_train, file_list_val, size_mult=32, nb_classes=21):
+        self.image_dir = image_dir
+        self.seg_dir = seg_dir
+        self.file_list_train = file_list_train
+        self.file_list_val = file_list_val
+        self.size_mult = size_mult
+        self.nb_classes = nb_classes
 
-def load_target_image(path, size_mult, nb_classes):
-    def load_palette_image(path):
-        img = Image.open(path)
-        img = np.array(img, dtype=np.uint8)
-        img[img==255] = 0
-        return img
-    image = tf.py_func(load_palette_image, [path], tf.uint8)
-    image = tf.one_hot(image, nb_classes, dtype=tf.uint8)
-    image.set_shape([None,None,nb_classes])
-    image = resize_image_to_multiple(image, size_mult)
-    return image
+        self.make_dataset_and_iterator()
+        self.make_file_lists()
 
-def get_loading_function(size_mult, nb_classes):
-    def load_image_pair(input_path, target_path):
-        input_image = load_input_image(input_path, size_mult)
-        target_image = load_target_image(target_path, size_mult, nb_classes) 
-        return input_image, target_image
-    return load_image_pair
+    def resize_image_to_multiple(self, image):
+        image_shape = tf.shape(image)
+        h = image_shape[0]
+        w = image_shape[1]
+        h = (h // self.size_mult) * self.size_mult
+        w = (w // self.size_mult) * self.size_mult    
+        image = tf.image.resize_images(image, [h, w])
+        return image
 
-def get_pascal_dataset(image_list_file, input_dir, target_dir, size_mult, nb_classes):
-    with open(image_list_file) as f:
-        image_list = f.read().splitlines()
-    input_files = [os.path.join(input_dir, im+'.jpg') for im in image_list]
-    target_files = [os.path.join(target_dir, im+'.png') for im in image_list]
+    def load_input_image(self, path):
+        image = tf.read_file(path)
+        image = tf.image.decode_jpeg(image, channels=3)
+        image = tf.image.convert_image_dtype(image, tf.float32)
+        image = self.resize_image_to_multiple(image)
+        return image
+
+    def load_target_image(self, path):
+
+        def load_palette_image(path):
+            img = Image.open(path)
+            img = np.array(img, dtype=np.uint8)
+            img[img==255] = 0
+            return img
+
+        image = tf.py_func(load_palette_image, [path], tf.uint8)
+        image = tf.one_hot(image, self.nb_classes, dtype=tf.uint8)
+        image.set_shape([None, None, self.nb_classes])
+        image = self.resize_image_to_multiple(image)
+        return image
+
+    def get_loading_function(self):
+        
+        def load_image_pair(input_path, target_path):
+            input_image = self.load_input_image(input_path)
+            target_image = self.load_target_image(target_path) 
+            return input_image, target_image
+        return load_image_pair
+
+    def make_dataset_and_iterator(self):
+        parse_function = self.get_loading_function()
+        self.img_ph = tf.placeholder(tf.string, [None,])
+        self.seg_ph = tf.placeholder(tf.string, [None,])
+        
+        self.dataset = tf.data.Dataset.from_tensor_slices((self.img_ph, self.seg_ph))
+        self.dataset = self.dataset.map(parse_function)
+        self.dataset = self.dataset.batch(1)
+        self.dataset = self.dataset.prefetch(10)
+
+        self.iterator = self.dataset.make_initializable_iterator()
+
+    def make_file_lists(self):
+
+        def make_one_list(file_list):
+            with open(file_list) as f:
+                files = f.read().splitlines()
+            img_files = [os.path.join(self.image_dir, im+'.jpg') for im in files]
+            seg_files = [os.path.join(self.seg_dir, im+'.png') for im in files]
+            return img_files, seg_files
+
+        self.img_files_train, self.seg_files_train = make_one_list(self.file_list_train)
+        self.img_files_val,  self.seg_files_val = make_one_list(self.file_list_val)
+        
+        self.nb_images_train = len(self.img_files_train)
+        self.nb_images_val = len(self.img_files_val)
+
     
-    nb_images = len(input_files)
-
-    parse_function = get_loading_function(size_mult, nb_classes)
-
-    dataset = tf.data.Dataset.from_tensor_slices((input_files, target_files))
-    dataset = dataset.shuffle(nb_images)
-    dataset = dataset.map(parse_function)
-    dataset = dataset.batch(1)
-    dataset = dataset.prefetch(10)
-    
-    return dataset, nb_images   
--- fcn/train.py
+++ fcn/train.py
@@ -1,12 +1,14 @@
+import numpy as np
 import tensorflow as tf
 from segmentation import data_iterator
 from segmentation.fcn import models
+import imageio
 
 sess = tf.Session()
 nb_classes = 21
 epochs = 100
 save_model_path = 'test'
-fcn_version = 8
+fcn_version = 32
 
 version_to_model = {
     8: models.fcn_8,
@@ -14,18 +16,43 @@ version_to_model = {
     32: models.fcn_32
 }
 
-dataset, nb_images = data_iterator.get_pascal_dataset(
-    '/Users/kazimpal/workspace/segmentation/VOC2012/ImageSets/Segmentation/train.txt', 
-    '/Users/kazimpal/workspace/segmentation/VOC2012/JPEGImages/', 
-    '/Users/kazimpal/workspace/segmentation/VOC2012/SegmentationClass/', 
-    fcn_version, 21)
+def color_map(N=256):
+    def bitget(byteval, idx):
+        return ((byteval & (1 << idx)) != 0)
 
-iter = dataset.make_one_shot_iterator()
-X, labels = iter.get_next()
+    cmap = np.zeros((N, 3), dtype='uint8')
+    for i in range(N):
+        r = g = b = 0
+        c = i
+        for j in range(8):
+            r = r | (bitget(c, 0) << 7-j)
+            g = g | (bitget(c, 1) << 7-j)
+            b = b | (bitget(c, 2) << 7-j)
+            c = c >> 3
+
+        cmap[i] = np.array([r, g, b])
+
+    return cmap
+
+cmap = color_map(21)
+
+def save_prediction(pred, path):
+    print(pred.dtype, np.amin(pred), np.amax(pred))
+    pred = np.argmax(pred, axis=-1)
+    print(pred.dtype, np.amin(pred), np.amax(pred))
+    pred = np.squeeze(cmap[pred])
+    print(pred.dtype, np.amin(pred), np.amax(pred))
+    imageio.imsave(path, pred)
+    print(path)
+
+pascal_dataset = data_iterator.PascalDataset(
+    '/Users/kazimpal/workspace/segmentation/VOC2012/JPEGImages/',
+    '/Users/kazimpal/workspace/segmentation/VOC2012/SegmentationClass/', 
+    '/Users/kazimpal/workspace/segmentation/VOC2012/ImageSets/Segmentation/train.txt',
+    '/Users/kazimpal/workspace/segmentation/VOC2012/ImageSets/Segmentation/minitest.txt')
+X, labels = pascal_dataset.iterator.get_next()
 
 prediction = version_to_model[fcn_version](sess, X, 21)
-print(prediction)
-input()
 
 loss = tf.losses.softmax_cross_entropy(labels, prediction)
 
@@ -37,10 +64,24 @@ with sess.as_default():
 
     print('starting training ... ')
     for epoch in range(1, epochs + 1):
-        for i in range(1, nb_images + 1):
+        
+        print('initializing iterator to training ... ')
+        sess.run(pascal_dataset.iterator.initializer, feed_dict= {pascal_dataset.img_ph: pascal_dataset.img_files_train, pascal_dataset.seg_ph:pascal_dataset.seg_files_train})
+        for i in range(1, pascal_dataset.nb_images_train + 1):
             l, _ = sess.run([loss, optimizer])
             if i % 10 == 0:
-                print('Epoch {}, Batch {}, Loss {:.3f}'.format(epoch, i, l))
+                print('Epoch {}, Batch {} of {}, Loss {:.3f}'.format(epoch, i, pascal_dataset.nb_images_train, l))
+        
+        print('initializing iterator to validation ... ')
+        sess.run(pascal_dataset.iterator.initializer, feed_dict= {pascal_dataset.img_ph: pascal_dataset.img_files_val, pascal_dataset.seg_ph:pascal_dataset.seg_files_val})
+        val_loss = 0
+        for i in range(1, pascal_dataset.nb_images_val + 1):
+            l, pred = sess.run([loss, prediction])
+            val_loss += l * 1.0/pascal_dataset.nb_images_val
+            pred_path = "test_{}_epoch_{}.png".format(i, epoch)
+            save_prediction(pred, pred_path)
+        print('Epoch {}, Val Loss {:.3f}'.format(epoch, val_loss))
+        
                     
     saver = tf.train.Saver()
     save_path = saver.save(sess, save_model_path)

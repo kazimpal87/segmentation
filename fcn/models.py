7bf6a110bddda2050c7c4325cb69bfc5278d53f5
kazimpal87 <kazim.pal@gmail.com>
2019-08-13 01:17:53 +0100

    wc

--- fcn/models.py
+++ fcn/models.py
@@ -1,15 +1,10 @@
 import tensorflow as tf
 import numpy as np
 
-def get_vgg16_pooling_layer(layer_num):
-    name = "vgg16/conv{}/pool/MaxPool:0".format(layer_num)
-    output = tf.get_default_graph().get_tensor_by_name(name)
-    return output
-
 def conv2d_helper(inputs, filters, kernel_size, name, do_dropout=True, activation='relu'):
     x = tf.keras.layers.Conv2D(filters, kernel_size, padding='same', activation=activation, name='seg/'+name)(inputs)
     if do_dropout:
-        x = tf.layers.dropout(x, name='seg/'+name+'_dropout')
+        x = tf.keras.layers.Dropout(rate=0.5, name='seg/'+name+'_dropout')(x)
     return x
 
 def bilinear_upsample_weights(factor, number_of_classes):
@@ -27,82 +22,97 @@ def bilinear_upsample_weights(factor, number_of_classes):
         weights[:, :, i, i] = upsample_kernel
     return weights
 
-def fcn_32(inputs, nb_classes):
-    vgg16_pretrained = tf.keras.applications.VGG16(include_top=False, input_tensor=inputs, weights='imagenet')
-    pool5 = vgg16_pretrained.output
-
-    # Replace fc layers with conv layers
-    conv6 = tf.keras.layers.Conv2D(4096, (7, 7), padding='same', activation='relu', name='seg/conv6')(pool5)
-    drop6 = tf.keras.layers.Dropout(rate=0.5)(conv6)
-    
-    conv7 = tf.keras.layers.Conv2D(4096, (1, 1), padding='same', activation='relu', name='seg/conv7')(drop6)
-    drop7 = tf.keras.layers.Dropout(rate=0.5)(conv7)
-    
-    conv7_score = tf.keras.layers.Conv2D(nb_classes, (1, 1), padding='same', activation='linear', name='seg/conv7_score')(drop7)
-    
-    logits = tf.keras.layers.Conv2DTranspose(
-        filters=nb_classes, 
-        kernel_size=(64, 64),
-        strides=(32, 32),
+def upsample_helper(inputs, filters, scale, name):
+    x = tf.keras.layers.Conv2DTranspose(
+        filters,
+        (scale*2, scale*2),
+        strides=(scale, scale),
         padding='same',
         activation='linear',
-        name='seg/final_score',
-        kernel_initializer=tf.keras.initializers.Constant(bilinear_upsample_weights(32, nb_classes)))(conv7_score)
-   
-    #softmax = tf.keras.activations.softmax(logits)
+        name='seg/'+name,
+        kernel_initializer=tf.keras.initializers.Constant(bilinear_upsample_weights(scale, filters)))(inputs)
+    return x
 
-    return logits, pool5, conv7_score
-'''
-def fcn_16(sess, inputs, nb_classes):    
-    vgg16_pretrained = nets.VGG16(inputs, is_training=True, stem=True)
-    sess.run(vgg16_pretrained.pretrained())
-    
-    # Replace fc layers with conv layers
-    conv6 = conv2d_helper(vgg16_pretrained, 4096, (7, 7), name='conv6')
+def fcn_32(inputs, nb_classes):
+    vgg16_pretrained = tf.keras.applications.VGG16(include_top=False, input_tensor=inputs, weights='imagenet')
+    pool5 = vgg16_pretrained.get_layer('block5_pool').output
+    conv6 = conv2d_helper(pool5, 4096, (7, 7), name='conv6')
     conv7 = conv2d_helper(conv6, 4096, (1, 1), name='conv7')
-    conv7_score = conv2d_helper(conv7, nb_classes, (1, 1), name='conv7_score', do_dropout=False)
+    conv8 = conv2d_helper(conv7, nb_classes, (1, 1), name='conv8', activation='linear', do_dropout=False)
+    logits = upsample_helper(conv8, nb_classes, 32, name='logits')
+    return logits
 
-    # Upsample conv7    
-    conv7_score_x2 = upsample_helper(conv7_score, nb_classes, 2, name='conv7_score_x2')
+def fcn_16(inputs, nb_classes):
+    vgg16_pretrained = tf.keras.applications.VGG16(include_top=False, input_tensor=inputs, weights='imagenet')
+    pool4 = vgg16_pretrained.get_layer('block4_pool').output
+    pool5 = vgg16_pretrained.get_layer('block5_pool').output
+    print(pool4)
+    print(pool5)
+    
+    conv6 = conv2d_helper(pool5, 4096, (7, 7), name='conv6')
+    conv7 = conv2d_helper(conv6, 4096, (1, 1), name='conv7')
+    conv8 = conv2d_helper(conv7, nb_classes, (1, 1), name='conv8', activation='linear', do_dropout=False)
+    print(conv6)
+    print(conv7)
+    print(conv8)
+
+    # Upsample conv8
+    conv8_up = upsample_helper(conv8, nb_classes, 2, name='conv8_up')
+    print(conv8_up)
 
     # Get pool4 and class scores
-    pool4 = get_vgg16_pooling_layer(4)
-    pool4_score = conv2d_helper(pool4, nb_classes, (1, 1), name='pool4_score')
+    pool4_scores = conv2d_helper(pool4, nb_classes, (1, 1), name='pool4_scores', activation='linear', do_dropout=False)
+    print(pool4_scores)
 
     # Compute sum
-    pool4_conv7_score = tf.math.add(pool4_score, conv7_score_x2)
+    sum1 = tf.math.add(pool4_scores, conv8_up, name='sum1')
+    print(sum1)
 
     # Upsample sum
-    final_score = upsample_helper(pool4_conv7_score, nb_classes, 16, name='final_score')
-    return final_score
+    logits = upsample_helper(sum1, nb_classes, 16, name='logits')
+    print(logits)
+    input()
 
-def fcn_8(sess, inputs, nb_classes):
-    vgg16_pretrained = nets.VGG16(inputs, is_training=True, stem=True)
-    sess.run(vgg16_pretrained.pretrained())
+    return logits
+
+def fcn_8(inputs, nb_classes):
+    vgg16_pretrained = tf.keras.applications.VGG16(include_top=False, input_tensor=inputs, weights='imagenet')
+    pool3 = vgg16_pretrained.get_layer('block3_pool').output
+    pool4 = vgg16_pretrained.get_layer('block4_pool').output
+    pool5 = vgg16_pretrained.get_layer('block5_pool').output
+    print(pool3)
+    print(pool4)
+    print(pool5)
     
     # Replace fc layers with conv layers
-    conv6 = conv2d_helper(vgg16_pretrained, 4096, (7,7), name='conv6')
-    conv7 = conv2d_helper(conv6, 4096, (1,1), name='conv7')
-    conv7_score = conv2d_helper(conv7, nb_classes, (1,1), name='conv7_score', do_dropout=False)
+    conv6 = conv2d_helper(pool5, 4096, (7, 7), name='conv6')
+    conv7 = conv2d_helper(conv6, 4096, (1, 1), name='conv7')
+    conv7_score = conv2d_helper(conv7, nb_classes, (1, 1), name='conv7_score', activation='linear', do_dropout=False)
+    print(conv6)
+    print(conv7)
+    print(conv7_score)
 
     # Upsample conv7    
     conv7_score_x4 = upsample_helper(conv7_score, nb_classes, 4, name='conv7_score_x4')
+    print(conv7_score_x4)
 
     # Get pool4 and class scores
-    pool4 = get_vgg16_pooling_layer(4)
-    pool4_score = conv2d_helper(pool4, nb_classes, (1,1), name='pool4_score')
+    pool4_score = conv2d_helper(pool4, nb_classes, (1,1), name='pool4_score', activation='linear', do_dropout=False)
     pool4_score_x2 = upsample_helper(pool4_score, nb_classes, 2, name='pool4_score_x2')
+    print(pool4_score)
+    print(pool4_score_x2)
 
     # Get pool3 and class scores
-    pool3 = get_vgg16_pooling_layer(3)
-    pool3_score = conv2d_helper(pool3, nb_classes, (1,1), name='pool3_score')
+    pool3_score = conv2d_helper(pool3, nb_classes, (1,1), name='pool3_score', activation='linear', do_dropout=False)
+    print(pool3_score)
 
     # Compute sum
     sum_score = tf.math.add_n([conv7_score_x4, pool4_score_x2, pool3_score])
+    print(sum_score)
 
     # Upsample sum
-    final_score = upsample_helper(sum_score, nb_classes, 8, name='final_score')
-    return final_score
-'''
+    logits = upsample_helper(sum_score, nb_classes, 8, name='logits')
+    print(logits)
 
+    return logits
 
--- fcn/train.py
+++ fcn/train.py
@@ -45,103 +45,49 @@ def save_y_pred(pred, path):
     imageio.imsave(path, pred)
     print(path)
 
-#pascal_dataset = data_iterator.PascalDataset(
-#    'C:\\Users\\Kazim\\workspace\\segmentation\\VOC2012\\JPEGImages\\',
-#    'C:\\Users\\Kazim\\workspace\\segmentation\\VOC2012\\SegmentationClass\\',
-#    'C:\\Users\\Kazim\\workspace\\segmentation\\VOC2012\\ImageSets\\Segmentation\\train.txt',
-#    'C:\\Users\\Kazim\\workspace\\segmentation\\VOC2012\\ImageSets\\Segmentation\\minitest.txt')
-
 pascal_dataset = data_iterator.PascalDataset(
-    '/Users/kazimpal/workspace/segmentation/VOC2012/JPEGImages',
-    '/Users/kazimpal/workspace/segmentation/VOC2012/SegmentationClass',
-    '/Users/kazimpal/workspace/segmentation/VOC2012/ImageSets/Segmentation/train.txt',
-    '/Users/kazimpal/workspace/segmentation/VOC2012/ImageSets/Segmentation/minitest.txt')
+    'C:\\Users\\Kazim\\workspace\\segmentation\\VOC2012\\JPEGImages\\',
+    'C:\\Users\\Kazim\\workspace\\segmentation\\VOC2012\\SegmentationClass\\',
+    'C:\\Users\\Kazim\\workspace\\segmentation\\VOC2012\\ImageSets\\Segmentation\\train.txt',
+    'C:\\Users\\Kazim\\workspace\\segmentation\\VOC2012\\ImageSets\\Segmentation\\minitest.txt')
 
-for e in range(5):
-    for k in range(1, pascal_dataset.nb_images_val + 1):
-        X, y = next(pascal_dataset.val_gen)
-        print(X.shape, y.shape)
-        plt.imshow(X[0,:,:,:].astype(np.uint8))
-        plt.show()
+#pascal_dataset = data_iterator.PascalDataset(
+#    '/Users/kazimpal/workspace/segmentation/VOC2012/JPEGImages',
+#    '/Users/kazimpal/workspace/segmentation/VOC2012/SegmentationClass',
+#    '/Users/kazimpal/workspace/segmentation/VOC2012/ImageSets/Segmentation/train.txt',
+#    '/Users/kazimpal/workspace/segmentation/VOC2012/ImageSets/Segmentation/minitest.txt')
 
 X = tf.placeholder(tf.float32, [None, None, None, 3])
 y = tf.placeholder(tf.float32, [None, None, None, nb_classes])
-y_pred, vgg_out, conv7_score = models.fcn_32(X, nb_classes)
+y_pred = models.fcn_8(X, nb_classes)
 
-y_flat = tf.keras.layers.Reshape((-1, nb_classes))(y)
-y_pred_flat = tf.keras.layers.Reshape((-1, nb_classes))(y_pred)
-
-loss = tf.losses.softmax_cross_entropy(onehot_labels=y_flat, logits=y_pred_flat)
-
+loss = tf.losses.softmax_cross_entropy(onehot_labels=y, logits=y_pred)
+optimizer = tf.train.AdamOptimizer(learning_rate=0.0001)
 train_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, "seg")
-print(train_vars)
-optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.0001).minimize(loss, var_list=train_vars)
+train_op = optimizer.minimize(loss, var_list=train_vars)
 
 with K.get_session() as sess:
     print('running global_variables_initializer')
-    #sess.run(tf.global_variables_initializer())
     sess.run(tf.variables_initializer (train_vars))
+    sess.run(tf.variables_initializer (optimizer.variables()))
 
     print('starting training ... ')
     for epoch in range(1, epochs + 1):
         
         for i in range(1, pascal_dataset.nb_images_train + 1):
             Xi, yi = next(pascal_dataset.train_gen)
-            _, l, yp, yt, yf = sess.run([optimizer, loss, y_pred, y, y_flat], feed_dict={X:Xi, y:yi})
-
-            foo = np.argmax(yp, axis=-1)
-            foo = np.squeeze(foo)
-
-            bar = np.argmax(yt, axis=-1)
-            bar = np.squeeze(bar)
-
-            mun = np.argmax(yf, axis=-1)
-            print(np.amax(mun))
-
-            plt.subplot(121)
-            plt.imshow(foo)
-            plt.subplot(122)
-            plt.imshow(bar)
-            plt.show()
-            #for c in range(21):
-            #    foo = yp[0,:,:,c]
-            #    plt.imshow(foo)
-            #    plt.show()
-            
-            #l2 = np.squeeze(l)
-            #plt.imshow(l2)
-            #plt.show()
-           
+            _, l, yp = sess.run([train_op, loss, y_pred], feed_dict={X:Xi, y:yi})
             yp = np.argmax(yp, axis=-1)
-            yp_min = np.amin(yp)
+            yt = np.argmax(yi, axis=-1)
+            yt_max = np.amax(yt)
             yp_max = np.amax(yp)
-
-            '''print(yf.shape, ypf.shape)
-            print(np.amin(yf), np.amax(yf))
-            print(np.amin(ypf), np.amax(ypf))
-            print(l.shape, np.amin(l), np.amax(l))
-            input()
-            count = 0
-            for k in range(ypf.shape[1]):
-                foo = yf[0,k,:]
-                bar = ypf[0,k,:]
-                if foo[0] == 1:
-                    continue
-                print(foo)
-                print(bar)
-                input()
-                count += 1
-                if count > 3:
-                    break'''
-            
-            l = np.mean(l)
-            print('Epoch {}, Batch {} of {}, Loss {:.3f}, yp_range {} {}'.format(epoch, i, pascal_dataset.nb_images_train, l, yp_min, yp_max))
+            print('Epoch {}, Batch {} of {}, Loss {:.3f}, yp_range {} {}'.format(epoch, i, pascal_dataset.nb_images_train, l, yt_max, yp_max))
         
         val_loss = 0
         for i in range(1, pascal_dataset.nb_images_val + 1):
             Xi, yi = next(pascal_dataset.val_gen)
             l, pi, yi = sess.run([loss, y_pred, y], feed_dict={X:Xi, y:yi})
-            l = np.mean(l)
+            
             val_loss += l * 1.0/pascal_dataset.nb_images_val
             
             pred_path = "test_{}_epoch_{}.png".format(i, epoch)
